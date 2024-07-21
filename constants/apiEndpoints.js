999d25e316670afd1532e4178890c81ad1bee44c
Sabbineni Jaswanth <98733861+Jaswanth169@users.noreply.github.com>
2024-07-22 01:04:35 +0530

    Update apiEndpoints.js

--- constants/apiEndpoints.js
+++ constants/apiEndpoints.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-    API_ENDPOINTS: {
-        BACKEND_PORT: process.env.BACKEND_PORT || 3000,
-        GET_ASSISTANT_RESPONSE: process.env.GET_ASSISTANT_RESPONSE || '/getAssistantResponse',
-    },
-    SUB_API_ENDPOINTS: {
-        CHATS: process.env.CHATS || '/chats',
-    }
+  API_ENDPOINTS: {
+    BACKEND_PORT: process.env.BACKEND_PORT || 3000,
+    GET_ASSISTANT_RESPONSE: process.env.GET_ASSISTANT_RESPONSE || '/getAssistantResponse',
+  },
+  SUB_API_ENDPOINTS: {
+    CHATS: process.env.CHATS || '/chats',
+  }
 };
