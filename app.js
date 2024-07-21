fadfee7155a9d0514386fd7b2050a27a3df0ad16
Sabbineni Jaswanth <98733861+Jaswanth169@users.noreply.github.com>
2024-07-22 00:39:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-// app.js
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 const chatRoute = require('./routes/chatRoute');
-const {API_ENDPOINTS} = require('./constants/apiEndpoints');
+const { API_ENDPOINTS } = require('./constants/apiEndpoints');
 
 const app = express();
-app.use(cors());
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(API_ENDPOINTS.GET_ASSISTANT_RESPONSE, chatRoute );
+app.use(API_ENDPOINTS.GET_ASSISTANT_RESPONSE, chatRoute);
 
 module.exports = app;
