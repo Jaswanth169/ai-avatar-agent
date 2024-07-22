d728ecf22f3c731aa0010f008695d250a43d0eed
Sabbineni Jaswanth <98733861+Jaswanth169@users.noreply.github.com>
2024-07-22 12:29:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');  // Import the app from app.js
 
-const PORT = process.env.PORT || 80;  // Use port 80 or the port defined in the environment variables
+const PORT = process.env.PORT || 8080;  // Use port 80 or the port defined in the environment variables
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
