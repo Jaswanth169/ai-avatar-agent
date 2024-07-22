f6a10862458caa12804a4392b0127e7050ca4481
Sabbineni Jaswanth <98733861+Jaswanth169@users.noreply.github.com>
2024-07-22 10:24:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const chatRoute = require('./routes/chatRoute');
+const cors = require('cors');
 const { API_ENDPOINTS } = require('./constants/apiEndpoints');
+const chatRoute = require('./routes/chatRoute');  // Import the routes from chatRoute.js
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());  // Enable CORS
+app.use(express.json());  // Parse JSON bodies
+app.use(express.urlencoded({ extended: true }));  // Parse URL-encoded bodies
 
+// Use the routes defined in chatRoute.js
 app.use(API_ENDPOINTS.GET_ASSISTANT_RESPONSE, chatRoute);
 
 module.exports = app;
